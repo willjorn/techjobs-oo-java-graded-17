511d39e9a6990d58669b7e82a2fdc9cd1e868fda
Willjorn <klpumagoi@gmail.com>
2024-11-04 21:28:27 -0600

    task 1 done

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,21 @@
 package org.launchcode.techjobs.oo;
 
+import static org.testng.Assert.assertNotEquals;
+
+
+import org.junit.Test;
+
 public class JobTest {
     //TODO: Create your unit tests here
-}
+
+
+        @Test
+        public void testSettingJobId() {
+            Job job1 = new Job();
+            Job job2 = new Job();
+
+            assertNotEquals(job1.getId(), job2.getId(), "Job IDs should be unique");
+        }
+    }
+
+
